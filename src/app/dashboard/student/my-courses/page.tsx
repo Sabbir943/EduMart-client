cbf66b748ee953640a7cb9336c76e575ce37da92
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-07-15 01:07:13 +0600

    set up by dashboard layout

---
+++ src/app/dashboard/student/my-courses/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyCouser = () => {
+    return (
+        <div>
+            <h1>My-cousre</h1>
+        </div>
+    );
+};
+
+export default MyCouser;
---
+++ src/app/dashboard/student/saved-courses/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SavedCourse = () => {
+    return (
+        <div>
+            <h1>Saved cousre</h1>
+        </div>
+    );
+};
+
+export default SavedCourse;
