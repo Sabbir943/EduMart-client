9eb4b28b1aaad278546d23f9d2a58f93ba3fb853
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-07-15 20:11:49 +0600

    fix the client url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "@/lib/auth"; // আপনার সার্ভার সাইড auth কনফিগ ফাইলের টাইপ ইম্পোর্ট করুন
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL:process.env.CLIENT_URL,
   plugins: [
     inferAdditionalFields<typeof auth>() // এটি সার্ভার কনফিগ থেকে 'role' ফিল্ডটির টাইপ ক্লায়েন্ট সাইডে যুক্ত করবে
   ]
